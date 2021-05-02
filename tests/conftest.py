f16b24b026368b4cd463d1699a3142285e8c3080
Andrii Kovalov <jfkwww1@gmail.com>
2021-05-02 15:14:06 +0000

    tests GET /courses endpoint, with and without params 'name','start_date','end_date'

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import os
 import sys
 from faker import Faker
 import random
+import json
 tests_folder_path = os.path.dirname(os.path.abspath(__file__)) + '/../'
 sys.path.append(tests_folder_path)
 from courses_crud import create_app # noqa
@@ -32,3 +33,21 @@ def generate_test_data():
     fake_course['start_date'] = fake.date()
     fake_course['end_date'] = fake.date()
     return fake_course
+
+
+def add_course(client, times=1):
+    for count in range(times):
+        test_user_data = generate_test_data()
+        response = client.post(
+            "/api/1.0/courses",
+            data=json.dumps(
+                {
+                    'name': test_user_data['title'],
+                    'lectures_count': test_user_data['lectures_count'],
+                    'start_date': test_user_data['start_date'],
+                    'end_date': test_user_data['end_date'],
+                }
+            ),
+            content_type="application/json",
+        )
+        response = json.loads(response.data)
